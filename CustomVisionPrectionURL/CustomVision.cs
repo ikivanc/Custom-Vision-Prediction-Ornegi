0034962d3f9412d14333716fd0ac3970e11cf59a
Ibrahim KIVANC <ikivanc@hotmail.com>
2018-01-03 00:17:27 +0300

    Initial commit

---
+++ CustomVisionPrectionURL/CustomVision.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomVisionPrectionURL
+{
+
+    public class CustomVision
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "Project")]
+        public string Project { get; set; }
+
+        [JsonProperty(PropertyName = "Iteration")]
+        public string Iteration { get; set; }
+
+        [JsonProperty(PropertyName = "Created")]
+        public DateTime Created { get; set; }
+
+        [JsonProperty(PropertyName = "Predictions")]
+        public Prediction[] Predictions { get; set; }
+    }
+
+    public class Prediction
+    {
+        [JsonProperty(PropertyName = "TagId")]
+        public string TagId { get; set; }
+
+        [JsonProperty(PropertyName = "Tag")]
+        public string Tag { get; set; }
+
+        [JsonProperty(PropertyName = "Probability")]
+        public float Probability { get; set; }
+    }
+
+}
---
+++ CustomVisionPrectionURL/Program.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomVisionPrectionURL
+{
+    static class Program
+    {
+        static void Main()
+        {
+            MakeRequest();
+            Console.WriteLine("Çıkmak için ENTER 'a basın...");
+            Console.ReadLine();
+        }
+
+        //Test için
+        //https://www.tarimdanhaber.com//media/img/7ca03b1f07fd6f3eaa4038ff9d69b197.jpg
+
+        static async void MakeRequest()
+        {
+            var client = new HttpClient();
+       
+            // Request headers
+            client.DefaultRequestHeaders.Add("Prediction-key", "25513706ccfd411a975b83d0d72c6e1d");
+
+            Console.Write("Enter image URL: ");
+            var imageUrl = Console.ReadLine();
+
+            var uri = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.1/Prediction/b03e07f6-9faa-4a91-b652-745467d3fbd4/url";
+            HttpResponseMessage response;
+
+            // Request body
+            byte[] byteData = Encoding.UTF8.GetBytes("{\"Url\": \"" + imageUrl + "\"}");
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response = await client.PostAsync(uri, content);
+                //Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+                //JSON Deserialization
+                string jsoncust = response.Content.ReadAsStringAsync().Result;
+                CustomVision custobjectVision = JsonConvert.DeserializeObject<CustomVision>(jsoncust);
+
+                //Search objesi için gerekli sonuçların alınması
+
+                Console.WriteLine("---\n\nCustom Vision Detayları");
+                foreach (Prediction p in custobjectVision.Predictions)
+                {
+                    Console.WriteLine(p.Tag + " - " + p.Probability);
+                }
+            }
+
+        }
+    }
+}
